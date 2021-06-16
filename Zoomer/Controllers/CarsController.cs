c9f391dad3638ddba7957a87bb570bb5111c5dbb
Brian Jones <brijones.nz@gmail.com>
2021-06-17 07:28:22 +1200

    Add project files.

---
+++ Zoomer/Controllers/CarsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zoomer.Models;
+using Zoomer.ViewModels;
+
+namespace Zoomer.Controllers
+{
+    public class CarsController : Controller
+    {
+        // GET: Cars
+        public ActionResult Home()
+        {
+            var car = new Car() { Make = "VW", Model = "Golf" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name="Customer A"},
+                new Customer {Name="Customer B"}
+            };
+            var viewModel = new RandomCarViewModel() 
+            { 
+                Car = car,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        } 
+    }
+}
---
+++ Zoomer/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zoomer.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string YearOfMake { get; set; }
+        public DateTime? DateAdded { get; set; }
+    }
+}
