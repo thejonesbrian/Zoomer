440cbb4b504f75a1658593a84786fc918d04a064
Brian Jones <brijones.nz@gmail.com>
2021-06-24 22:56:29 +1200

    Adding Validation to View and styling it in CSS

--- Zoomer/Controllers/CustomersController.cs
+++ Zoomer/Controllers/CustomersController.cs
@@ -34,6 +34,15 @@ namespace Zoomer.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new NewCustomerViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("New", viewModel);
+            }
             if(customer.Id == 0)
                 _context.Customers.Add(customer);
             else
