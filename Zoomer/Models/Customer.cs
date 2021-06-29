9feb9e9bb8847631b9fdcdfe4d8157243502598a
Brian Jones <brijones.nz@gmail.com>
2021-06-29 22:42:31 +1200

    Apply a min age needed to rent a car

--- Zoomer/Models/Customer.cs
+++ Zoomer/Models/Customer.cs
@@ -16,6 +16,7 @@ namespace Zoomer.Models
         public MembershipType MembershipType { get; set; }
         [Display(Name="Membership Type")]
         public byte MembershipTypeId { get; set; }
+        [MinAgeToDriveCarWithLicense]
         public DateTime? Birthdate { get; set; }
     }
 }
---
+++ Zoomer/Models/MinAgeToDriveCarWithLicense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Zoomer.Models
+{
+    public class MinAgeToDriveCarWithLicense : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birth date is required.");
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+            return (age > 15 || age == 15) ? ValidationResult.Success : new ValidationResult("Customer is to young to hold a license and can not drive.");
+        }
+    }
+}
